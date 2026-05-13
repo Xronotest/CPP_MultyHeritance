cdae6bb1679c959d9fb81d37272b57d6dfb15c7d
Top <Top@WIN-4NHMOPJIR1M>
2026-05-13 10:56:03 +0500

    Добавление UML диаграммы

---
+++ Testing/Testing.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "..\CPP_MultyHeritance\SceneCreator.hpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testing
+{
+	TEST_CLASS(SceneCreator)
+	{
+	public:
+		
+		TEST_METHOD(CreateGame)
+		{
+		}
+	};
+}
